7b35113c8a90c9a77328c495ec734e75d2124a7b
mccannem <156856358+mccannem@users.noreply.github.com>
2024-04-07 20:21:22 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let slideIndex = [1,1,1];
+let slideId = ["pet-prev", "rec-prev", "trav-prev"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 2) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+
+
